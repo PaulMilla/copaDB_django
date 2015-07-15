4c24d8f1d158d791d69eed313c885911ee1acabf
PaulMilla <paul.milla1@gmail.com>
2015-07-15 06:17:43 +0000

    Added logging support

--- copaDB/settings.py
+++ copaDB/settings.py
@@ -118,3 +118,32 @@ ADMIN_STATIC_PREFIX = '/static/admin'
 #STATICFILES_DIRS = (
     #os.path.join(BASE_DIR, 'staticfiles'),
 #)
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/logs/copaDB.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'www': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
--- printAllModels.py
+++ printAllModels.py
@@ -3,3 +3,9 @@ from www.models import Match, Country, Player
 list(Player.objects.all())
 list(Country.objects.all())
 list(Match.objects.all())
+
+
+c = Country.objects.get(country="Chile"
+p = Player.objects.get(player="Claudio Bravo")
+c.captain
+c.captain = p
